34b5622339fca384f60bdffc89c40afd67240419
jonathan11ch <jonathan11ch@hotmail.com>
2019-05-08 00:40:13 -0500

    added basic functionality

--- botbuilder_workbench/scripts/module.py
+++ botbuilder_workbench/scripts/module.py
@@ -1,26 +1,51 @@
+#!/usr/bin/env python
 import rospy
 import time
 
-import actuator.Actuator as Actuator
+#import actuator.Actuator as Actuator
 
 #base class for modules
 class Module(object):
-
-	def __init__(self, module_type = "dof", ):
-		self.type = module_type  
+	'''Module base class'''
+	def __init__(self, args):
+		self.name = args
+		self.components = []
+		pass
 		
-		self.dof = Actuator()
+	def print_name(self):
+		print "Oject class: " +  str(type(self)) + " my name is: " + self.name
+					
+	def register_component(self, component):
+		self.components.append(component)
+		pass
+		
+		
 
-
-
-
-
-
-
-
-
-class Sensor(object):
+class Assembler(object):
+	'''Assembler class to create the robot modules based on yaml files'''
 	def __init__(self):
-		self.type = "capacitive"
+		rospy.init_node("robot_builder")
+		self.rate = rospy.Rate(10)
+		self.modules = []
+		self.load_robot_file()
+		
+	
+	def load_robot_file(self):
+		self.robot = rospy.get_param("robot")
+		print self.robot['physical_modules']
+		for module in self.robot['physical_modules']:
+			self.modules.append(Module(module))
+			print "###################################################"
+			print self.robot['physical_modules'][module]['components']
+			print "###################################################"
+		
+		for m in self.modules:
+			m.print_name()
+			
+		
+		
 
-		self.value = ""
+a = Assembler()
+time.sleep(3)
+a.load_robot_file()
+
